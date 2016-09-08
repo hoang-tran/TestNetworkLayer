35effb63b4276d7db1ce6e5e1f9c409b93593b7d
Hoang Tran <hoangtx.master@gmail.com>
2016-09-08 21:30:11 +0700

    add first spec

---
+++ TestNetworkLayerTests/MyApiClientSpec.swift
@@ -0,0 +1,22 @@
+//
+//  MyApiClientSpec.swift
+//  TestNetworkLayer
+//
+//  Created by Hoang Tran on 9/8/16.
+//  Copyright © 2016 Hoang Tran. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class MyApiClientSpec : QuickSpec {
+  override func spec() {
+    super.spec()
+
+    describe("first test") {
+      it("should pass") {
+        expect(1).to(equal(1))
+      }
+    }
+  }
+}
